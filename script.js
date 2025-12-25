b3cfc7da502874ff891cceaf7e70289001f26abe
LCrowe42 <LNCrowe@outlook.com>
2025-12-24 23:01:20 -0500

    Some basic formatting and addition of scripts and styles

---
+++ script.js
@@ -0,0 +1,21 @@
+function getCurrentDay() {
+    const today = new Date();
+    console.log(today.getDate());
+    return today;
+}
+
+function getCurrentMonth() {
+    const today = new Date();
+    console.log(today.getMonth() + 1);
+    return today;
+}
+
+function getCurrentYear() {
+    const today = new Date();
+    console.log(today.getFullYear());
+    return today;
+}
+
+function calculateAge() {
+    
+}
