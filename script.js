1d34e6e8bd4022fd943abf4fc3a29dc484f5c088
LCrowe42 <LNCrowe@outlook.com>
2025-12-27 22:05:59 -0500

    Created javascript function to calculate age and change output elements

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-function getCurrentDay() {
-    const today = new Date();
-    console.log(today.getDate());
-    return today;
-}
-
-function getCurrentMonth() {
-    const today = new Date();
-    console.log(today.getMonth() + 1);
-    return today;
-}
-
-function getCurrentYear() {
-    const today = new Date();
-    console.log(today.getFullYear());
-    return today;
-}
-
 function calculateAge() {
-    
+    const dayin = parseInt(document.getElementById("dayin").value,10);
+    const monthin = parseInt(document.getElementById("monthin").value,10);
+    const yearin = parseInt(document.getElementById("yearin").value,10);
+
+    const birth = new Date(yearin,monthin-1,dayin)
+    const today = new Date();
+
+    var years = today.getFullYear() - birth.getFullYear();
+    var months = today.getMonth() - birth.getMonth();
+    var days = today.getDate() - birth.getDate();
+    if (days < 0) {
+        const previousMonth = new Date(today.getFullYear(),today.getMonth(),0);
+        days += previousMonth.getDate();
+        months--;
+    }
+    if (months < 0) {
+        months += 12
+        years--;
+    }
+
+    document.getElementById("yearsout").textContent = (years).toString();
+    document.getElementById("monthsout").textContent = (months).toString();
+    document.getElementById("daysout").textContent = (days).toString();
+    return false;
 }
