688c61e10871f2350e1532a4c6824174dab0c9e3
LCrowe42 <LNCrowe@outlook.com>
2025-12-28 00:05:58 -0500

    Multiple layout/design changes created event listener to replace onclick added select inputs for month and year

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const submit = document.getElementById("formsubmit");
+    submit.addEventListener("click", calculateAge);
+});
+
+window.onload = function() {
+    const currentYear = new Date().getFullYear();
+    const startYear = 1900;
+    const yearSelect = document.getElementById("yearin");
+
+    for (let i = currentYear; i >= startYear; i--) { // loops through the years and add options
+        let option = document.createElement("option");
+        option.value = i;
+        option.textContent = i;
+        yearSelect.appendChild(option);
+    }
+
+    const monthSelect = document.getElementById("monthin");
+    for (let i = 12; i >= 1; i--) {
+        let option = document.createElement("option");
+        option.value = i;
+        option.textContent = i;
+        monthSelect.appendChild(option);
+    }
+};
+
 function calculateAge() {
     const dayin = parseInt(document.getElementById("dayin").value,10);
     const monthin = parseInt(document.getElementById("monthin").value,10);
     const yearin = parseInt(document.getElementById("yearin").value,10);
+
+    if (dayin > 31 || dayin < 1) {
+        return false;
+    }
 
     const birth = new Date(yearin,monthin-1,dayin)
     const today = new Date();
@@ -18,7 +48,6 @@ function calculateAge() {
         months += 12
         years--;
     }
-
     document.getElementById("yearsout").textContent = (years).toString();
     document.getElementById("monthsout").textContent = (months).toString();
     document.getElementById("daysout").textContent = (days).toString();
